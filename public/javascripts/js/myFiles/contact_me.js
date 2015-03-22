c0f8f4006dbfb1c4778d1e5df416182b1302ca83
Divneet Sethi <divneet91@yahoo.com>
2015-03-22 01:13:43 -0400

    home page and foundation files added

---
+++ public/javascripts/js/myFiles/contact_me.js
@@ -0,0 +1,39 @@
+//File Name: contact_me.js
+//Author's Name: Divneet Sethi
+//Website Name: Divneet Sethi Personal Portfolio (divneetsethi.github.io) 
+//File Description: javascript to validate the fields of the form if they are null or not. then send the mail.
+
+function validateContactForm() {
+
+    var subject = document.getElementById("txtSubject").value;
+    var name = document.getElementById("txtName").value;
+    var email = document.getElementById("txtEmail").value;
+    var message = document.getElementById("txtMessage").value;
+
+    if(subject==null || subject=="") {
+        alert("Enter subject");
+    }
+    else if (name == null || name == "") {
+        alert("Enter name");
+    }
+    else if (email == null || email == "") {
+        alert("Enter email");
+    }
+    else if (message == null || message == "") {
+        alert("Enter message");
+    }
+    else {
+        sendMail();
+    }
+}
+
+function sendMail() {
+    
+    var subject = document.getElementById("txtSubject").value;
+    var name = document.getElementById("txtName").value;
+    var email = document.getElementById("txtEmail").value;
+    var message = document.getElementById("txtMessage").value;
+
+    var body = "Name: " + name + "%0DEmail: " + email + "%0D%0D" + message;
+    document.getElementById("btnSendMail").href = "mailto:divneet91@yahoo.com?subject="+subject+"&body="+body;
+}
