821271bd91f5603d503765b55a165c8ae598c083
Divneet Sethi <divneet91@yahoo.com>
2015-03-22 19:26:57 -0400

    partials added and index.js modified

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '' });
+  res.render('index', { title: 'home' });
+});
+
+/* GET about me page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'about' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'projects' });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'services' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'contact' });
 });
 
 module.exports = router;
