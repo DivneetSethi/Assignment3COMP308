ec3e8a79c3a8409be4f1c6544dd10f34d6fdcbde
Divneet Sethi <divneet91@yahoo.com>
2015-03-22 20:16:19 -0400

    Partils added and views updated

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,37 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'home' });
+  res.render('index', { title: 'Home' });
 });
 
 /* GET about me page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'about' });
+  res.render('index', { title: 'About Me' });
 });
 
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'projects' });
+  res.render('index', { title: 'Projects' });
 });
 
 /* GET services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'services' });
+  res.render('index', { title: 'Services' });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'contact' });
+  res.render('index', { title: 'Contact Me' });
+});
+
+/* GET terms and conditions page. */
+router.get('/terms', function(req, res, next) {
+  res.render('index', { title: 'Terms and Conditions' });
+});
+
+/* GET privacy policy page. */
+router.get('/privacy', function(req, res, next) {
+  res.render('index', { title: 'Privacy Policy' });
 });
 
 module.exports = router;
